993093420651b8127e03ab131a43908f17e6c179
lipnitsky93 <lipnitsky93@yandex.ru>
2018-06-17 23:18:39 +0300

    The-Game: added new audio files

--- js/listening.js
+++ js/listening.js
@@ -9,7 +9,25 @@ const listeningArr = {
     './audio/listening/hide.wav': 'hide',
     './audio/listening/hunt.wav': 'hunt',
     './audio/listening/middle.wav': 'middle',
-    './audio/listening/moment.wav': 'moment'
+    './audio/listening/moment.wav': 'moment',
+    './audio/listening/adventure.wav': 'adventure',
+    './audio/listening/approach.wav': 'approach',
+    './audio/listening/chemical.wav': 'chemical',
+    './audio/listening/create.wav': 'create',
+    './audio/listening/evil.wav': 'evil',
+    './audio/listening/experiment.wav': 'experiment',
+    './audio/listening/kill.wav': 'kill',
+    './audio/listening/laboratory.wav': 'laboratory',
+    './audio/listening/laugh.wav': 'laugh',
+    './audio/listening/loud.wav': 'loud',
+    './audio/listening/nervous.wav': 'nervous',
+    './audio/listening/noise.wav': 'noise',
+    './audio/listening/project.wav': 'project',
+    './audio/listening/scare.wav': 'scare',
+    './audio/listening/secret.wav': 'secret',
+    './audio/listening/shout.wav': 'shout',
+    './audio/listening/smell.wav': 'smell',
+    './audio/listening/terrible.wav': 'terrible',
 };
 
 export {listeningArr};
